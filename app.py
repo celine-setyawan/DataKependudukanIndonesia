4ee19dc309f2e9c3f2b86dc6fd9d7f7b2e1c99e5
chrysant.celine <chrysant.celine@ui.ac.id>
2020-08-23 17:07:29 +0700

    Update dashboard

--- app.py
+++ app.py
@@ -31,9 +31,13 @@ options_list = list(df_choropleth.columns[1:])
 
 path_arrow = './assets/up-arrow.png'
 path_new = './assets/new.png'
+path_question = './assets/question.png'
+path_graph = './assets/graph.png'
 
 encoded_up_arrow = base64.b64encode(open(path_arrow, 'rb').read())
 encoded_new = base64.b64encode(open(path_new, 'rb').read())
+encoded_question = base64.b64encode(open(path_question, 'rb').read())
+encoded_graph = base64.b64encode(open(path_graph, 'rb').read())
 
 
 # ------------------------------------------------------------------------------
@@ -150,7 +154,7 @@ def create_line_chart():
 
 app.layout = html.Div([
     dbc.Container([
-        html.H2('Data Kependudukan Indonesia per Provinsi', style={'font-weight': 'bold', 'margin-left': '10%'}),
+        html.H2('Jumlah Penduduk Indonesia per Provinsi', style={'font-weight': 'bold', 'margin-left': '10%'}),
         dbc.Row([
             dbc.Col([
                 html.H1('3', style={'margin': '10px', 'font-size': '80px', 'color': '#B21727'}),
@@ -160,17 +164,21 @@ app.layout = html.Div([
                     html.Span('berada di '),
                     html.B('Pulau Jawa '),
                     html.Br(),
-                    html.Span('(saat ini di atas 34 juta)'),
+                    html.Span('(saat ini di atas 34 juta).'),
                     html.Br(),
                     html.Br(),
-                    html.Span('Penduduk di provinsi lainnya berjumlah di bawah 15 juta'),
+                    html.Span('Berdasarkan '),
+                    html.A('Wikipedia, ', href='https://en.wikipedia.org/wiki/List_of_islands_by_population'),
+                    html.Span('Pulau Jawa merupakan '),
+                    html.B('pulau dengan jumlah penduduk terbanyak di dunia.'),
+                    html.Br(),
+                    html.Br(),
                 ], style={'margin-left': '10%', 'margin-right': '10%'}),
             ], style={'background': 'white',
                 'border-radius': '5px',
                 'border': '1px solid #BEC2C4',
                 'box-shadow': '3px 3px 5px #B21727',
-                'width': '500px',
-                'height': '300px',
+                'width': '33%',
                 'text-align': 'center'}
             ),
             dbc.Col([
@@ -184,14 +192,13 @@ app.layout = html.Div([
                     html.Br(),
                     html.Br(),
                     html.Span('Sedangkan provinsi lainnya cenderung landai.'),
-                ], style={'margin-top': '10px', 'margin-left': '10%', 'margin-right': '10%'}),
+                ], style={'margin-top': '15%', 'margin-left': '10%', 'margin-right': '10%'}),
             ], style={'background': 'white',
                 'border-radius': '5px',
                 'border': '1px solid #BEC2C4',
                 'box-shadow': '3px 3px 5px #B21727',
-                'width': '500px',
-                'height': '300px',
-                'margin-left': '20px',
+                'width': '33%',
+                'margin-left': '3%',
                 'text-align': 'center'}
             ),
             dbc.Col([
@@ -200,19 +207,21 @@ app.layout = html.Div([
                 html.P(children=[
                     html.Span('Jumlah penduduk di beberapa provinsi masih sedikit, beberapa di antaranya karena merupakan '),
                     html.B('provinsi baru'),
-                    html.Span(', seperti '),
-                    html.B('Kalimantan Utara.'),
+                    html.Span(', seperti Kalimantan Utara, Papua Barat, dan Sulawesi Barat.'),
                     html.Br(),
                     html.Br(),
-                    html.Span('Terlepas dari provinsi baru, penduduk provinsi di luar Pulau Jawa cenderung lebih sedikit.'),
-                ], style={'margin-top': '35px', 'margin-left': '10%', 'margin-right': '10%'}),
+                    html.Span('Terlepas dari provinsi baru atau bukan, penduduk provinsi di '),
+                    html.B('luar Pulau Jawa cenderung lebih sedikit '),
+                    html.Span('(berjumlah di bawah 15 juta).'),
+                    html.Br(),
+                    html.Br()
+                ], style={'margin-top': '15%', 'margin-left': '10%', 'margin-right': '10%'}),
             ], style={'background': 'white',
                 'border-radius': '5px',
                 'border': '1px solid #BEC2C4',
                 'box-shadow': '3px 3px 5px #B21727',
-                'width': '500px',
-                'height': '300px',
-                'margin-left': '20px',
+                'width': '33%',
+                'margin-left': '3%',
                 'text-align': 'center'}
             ),
         ], style={'margin-top': '20px', 'margin-left': '10%',
@@ -244,12 +253,103 @@ app.layout = html.Div([
                 'border-radius': '5px',
                 'border': '1px solid #BEC2C4',
                 'box-shadow': '3px 3px 5px #B21727',
-                'width': '1200px',
+                'width': '100%',
                 'height': '450px',
                 'margin-top': '20px',
                 'margin-left': '10%',
+                'margin-right': '10%',
                 'display': 'flex',
                 'justify-content': 'space-between'}
+        ),
+
+        dbc.Row([
+            dbc.Col([
+                html.H1('10', style={'margin': '10px', 'font-size': '80px', 'color': '#B21727'}),
+                html.P(children=[
+                    html.Span('Provinsi dengan '),
+                    html.B('jumlah penduduk terbanyak: '),
+                    html.Br(),
+                    html.Span('(per tahun 2019)'),
+                    html.Br(),
+                    html.Br(),
+                ], style={'margin-left': '10%', 'margin-right': '10%'}),
+                html.P(children=[
+                    html.Span('1. Jawa Barat (49.3 juta)'),
+                    html.Br(),
+                    html.Span('2. Jawa Timur (39.6 juta)'),
+                    html.Br(),
+                    html.Span('3. Jawa Tengah (34.7 juta)'),
+                    html.Br(),
+                    html.Span('4. Sumatera Utara (14.5 juta)'),
+                    html.Br(),
+                    html.Span('5. Banten (12.9 juta)'),
+                    html.Br(),
+                    html.Span('6. DKI Jakarta (10.5 juta)'),
+                    html.Br(),
+                    html.Span('7. Sulawesi Selatan (8.8 juta)'),
+                    html.Br(),
+                    html.Span('8. Sumatera Selatan (8.47 juta)'),
+                    html.Br(),
+                    html.Span('9. Lampung (8.44 juta)'),
+                    html.Br(),
+                    html.Span('10. Riau (6.9 juta)'),
+                    html.Br(),
+                ], style={'margin-left': '10%', 'margin-right': '10%', 'text-align': 'left'}),
+            ], style={'background': 'white',
+                'border-radius': '5px',
+                'border': '1px solid #BEC2C4',
+                'box-shadow': '3px 3px 5px #B21727',
+                'width': '33%',
+                'text-align': 'center'}
+            ),
+            dbc.Col([
+                html.Img(src='data:image/png;base64,{}'.format(encoded_graph.decode()), height=100,
+                         style={'margin-top': '30px'}),
+                html.P(children=[
+                    html.Span('Semua provinsi terus mengalami peningkatan jumlah penduduk secara linear, hanya '),
+                    html.B('sedikit yang pernah mengalami penurunan.'),
+                    html.Br(),
+                    html.Br(),
+                    html.Span('Penurunan penduduk yang paling terlihat adalah di provinsi '),
+                    html.B('Jawa Barat pada tahun 1995-2000*.'),
+                    html.Br(),
+                    html.Br()
+                ], style={'margin-top': '15%', 'margin-left': '10%', 'margin-right': '10%'}),
+                html.P('*Penyebab masih belum diketahui.', style={'font-size': '10px'})
+            ], style={'background': 'white',
+                'border-radius': '5px',
+                'border': '1px solid #BEC2C4',
+                'box-shadow': '3px 3px 5px #B21727',
+                'width': '33%',
+                'margin-left': '3%',
+                'text-align': 'center'}
+            ),
+            dbc.Col([
+                html.Img(src='data:image/png;base64,{}'.format(encoded_question.decode()), height=80,
+                         style={'margin-top': '30px'}),
+                html.P(children=[
+                    html.Span('Banyaknya penduduk di Pulau Jawa disebabkan karena '),
+                    html.B('migrasi yang tinggi dan fasilitas kesehatan* yang baik. '),
+                    html.Br(),
+                    html.Br(),
+                    html.Span('Pusat ekonomi dan pemerintahan yang dominan berada di Pulau Jawa '),
+                    html.Span('menimbulkan efek domino, di mana '),
+                    html.B('fasilitas pendidikan, kesehatan, dan akses internet '),
+                    html.Span('di Pulau Jawa juga lebih memadai dibandingkan pulau lainnya.'),
+                    html.Br(),
+                    html.Br()
+                ], style={'margin-top': '5%', 'margin-left': '10%', 'margin-right': '10%'}),
+                html.P('*mendukung kelahiran dan mencegah kematian', style={'font-size': '10px'})
+            ], style={'background': 'white',
+                'border-radius': '5px',
+                'border': '1px solid #BEC2C4',
+                'box-shadow': '3px 3px 5px #B21727',
+                'width': '33%',
+                'margin-left': '3%',
+                'text-align': 'center'}
+            )
+        ], style={'margin-top': '20px', 'margin-left': '10%',
+                  'display': 'flex', 'justify-content': 'space-between'}
         ),
 
         dbc.Row([
@@ -265,10 +365,11 @@ app.layout = html.Div([
                 'border-radius': '5px',
                 'border': '1px solid #BEC2C4',
                 'box-shadow': '3px 3px 5px #B21727',
-                'width': '1200px',
+                'width': '100%',
                 'height': '450px',
                 'margin-top': '20px',
                 'margin-left': '10%',
+                'margin-right': '10%',
                 'text-align': 'center'}
         ),
 
@@ -279,7 +380,8 @@ app.layout = html.Div([
         html.A('Code on Github', href='https://github.com/celineinc/DataKependudukanIndonesia'),
         html.A('Data Penduduk', href='https://www.bps.go.id/indikator/indikator/view_data_pub/0000/api_pub/50/da_03/1'),
         html.A('Data GeoJSON', href='https://github.com/superpikar/indonesia-geojson')
-    ], style={'margin-left': '10%', 'display': 'flex', 'justify-content': 'space-evenly'})
+    ], style={'margin-left': '10%', 'margin-right': '10%',
+              'display': 'flex', 'justify-content': 'space-evenly'})
 ])
 
 
